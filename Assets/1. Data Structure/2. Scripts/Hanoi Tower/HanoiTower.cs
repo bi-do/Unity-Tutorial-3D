487633cc230328f11bf96c71e69a1891d4f70d05
ys kim <tbvjdkfdir@gmail.com>
2025-07-11 16:08:10 +0900

    [ADD] Unity BootCamp Day 38 - Algorithm
    [Algorithm]
    - 탐색
      - 순차 탐색
      - 이진 탐색
      - 깊이 우선 탐색 ( DFS )
      - 너비 우선 탐색 ( BFS )
    
    - 트리
      - 이진 탐색 트리 ( Binary Tree )
    
    [Unity]
    - 하노이 타워 정답 알고리즘
    
    [C#]
    - 중첩 클래스
    
    2025/07/11

--- Assets/1. Data Structure/2. Scripts/Hanoi Tower/HanoiTower.cs
+++ Assets/1. Data Structure/2. Scripts/Hanoi Tower/HanoiTower.cs
@@ -3,20 +3,27 @@ using TMPro;
 using Unity.Burst.Intrinsics;
 using UnityEngine;
 using UnityEngine.ProBuilder.Shapes;
+using UnityEngine.UI;
 
 public class HanoiTower : MonoBehaviour
 {
     public enum HanoiLv { Lv1 = 3, Lv2, Lv3 };
-    public HanoiLv hanoi_lv;
+    public HanoiLv hanoi_lv = HanoiLv.Lv1;
     public GameObject[] donut_prefab;
     public BarStack[] bars;
 
     public TextMeshProUGUI cnt_text;
+    public Button answer_btn;
 
     public static GameObject cur_donut = null;
     public static BarStack cur_bar = null;
     public static bool isSelected;
     public static int move_cnt;
+
+    void Awake()
+    {
+        this.answer_btn.onClick.AddListener(HanoiAnswer);
+    }
 
     void Start()
     {
@@ -50,5 +57,27 @@ public class HanoiTower : MonoBehaviour
         this.cnt_text.text = move_cnt.ToString();
     }
 
+    public void HanoiAnswer()
+    {
+        HanoidRoutine((int)this.hanoi_lv, 0, 1, 2);
+    }
+
+    private void HanoidRoutine(int param_cnt, int param_from, int param_temp, int param_to)
+    {
+        if (param_cnt == 0)
+            return;
+
+        if (param_cnt == 1)
+        {
+            Debug.Log($"{param_cnt}번 도넛을 {param_from}에서 {param_to}로 이동");
+        }
+        else
+        {
+            HanoidRoutine(param_cnt - 1, param_from, param_to, param_temp);
+            Debug.Log($"{param_cnt}번 도넛을 {param_from}에서 {param_to}로 이동");
+            HanoidRoutine(param_cnt - 1, param_temp, param_from, param_to);
+        }
+
+    }
 
 }
